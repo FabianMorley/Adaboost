616d02c013aea34bb34be61e1a0a290d3ffa8743
Fabian <fabian.morley@hotmail.co.uk>
2021-05-18 13:07:01 +0100

    minor optimisation

--- src/camera/Haar/RunNewHaar.java
+++ src/camera/Haar/RunNewHaar.java
@@ -100,7 +100,6 @@ public class RunNewHaar {
      */
     public int featureSum(int[][] int_img_mat, int x, int y, HaarFeature feature){
         //TODO: Might need to -1 from x and y if array out of bounds error not sure yet
-
         //readIm(); // This reads the resized image so we can use its pixels later
 
         // INTEGRAL CALCULATION
@@ -110,110 +109,134 @@ public class RunNewHaar {
         int width = feature.matrix.length;
         int height = feature.matrix[0].length;
         //System.out.println("Haar Feature: " + width + " x " + height);
-        switch(feature.feature_type){
-            case 1:
-                // 1 = [1,0]
-                int bA = int_img_mat[x][y];
-                int bB = int_img_mat[x][y+(width/2)-1];
-                int bC = int_img_mat[x+height-1][y+(width/2)-1];
-                int bD = int_img_mat[x+height-1][y];
-                int black = bC+bA-bB-bD; // Do I divide this by number of pixels in this area..?
+        try {
+            switch (feature.feature_type) {
+                case 1:
+                    // 1 = [1,0]
+                    // FOR SCALE 1 CASE
+                    if(width == 2 && height == 1){
+                        return int_img_mat[x][y] - int_img_mat[x][y+1];
+                    }
+                    int bA = int_img_mat[x][y];
+                    int bB = int_img_mat[x][y + (width / 2) - 1];
+                    int bC = int_img_mat[x + height - 1][y + (width / 2) - 1];
+                    int bD = int_img_mat[x + height - 1][y];
+                    int black = bC + bA - bB - bD; // Do I divide this by number of pixels in this area..?
 
-                int wA = int_img_mat[x][y+(width/2)];
-                int wB = int_img_mat[x][y+width-1];
-                int wC = int_img_mat[x+height-1][y+width-1];
-                int wD = int_img_mat[x+height-1][y];
-                int white = wC+wA-wB-wD;
+                    int wA = int_img_mat[x][y + (width / 2)];
+                    int wB = int_img_mat[x][y + width - 1];
+                    int wC = int_img_mat[x + height - 1][y + width - 1];
+                    int wD = int_img_mat[x + height - 1][y];
+                    int white = wC + wA - wB - wD;
 
-                return (black-white)/(width*height);
-            case 2:
-                // 2 = [1]
-                //     [0]
-                int b2A = int_img_mat[x][y];
-                int b2B = int_img_mat[x][y+width-1];
-                int b2C = int_img_mat[x+(height/2)-1][y+width-1];
-                int b2D = int_img_mat[x+(height/2)-1][y];
-                int black2 = b2C+b2A-b2B-b2D;
+                    return (black - white) / (width * height);
+                case 2:
+                    // 2 = [1]
+                    //     [0]
+                    // FOR SCALE 1 CASE
+                    if(width == 1 && height == 2){
+                        return int_img_mat[x][y]-int_img_mat[x+1][y];
+                    }
+                    int b2A = int_img_mat[x][y];
+                    int b2B = int_img_mat[x][y + width - 1];
+                    int b2C = int_img_mat[x + (height / 2) - 1][y + width - 1];
+                    int b2D = int_img_mat[x + (height / 2) - 1][y];
+                    int black2 = b2C + b2A - b2B - b2D;
 
-                int w2A = int_img_mat[x+(height/2)][y];
-                int w2B = int_img_mat[x+(height/2)][y+width-1];
-                int w2C = int_img_mat[x+height-1][y+width-1];
-                int w2D = int_img_mat[x+height-1][y];
-                int white2 = w2C+w2A-w2B-w2D;
+                    int w2A = int_img_mat[x + (height / 2)][y];
+                    int w2B = int_img_mat[x + (height / 2)][y + width - 1];
+                    int w2C = int_img_mat[x + height - 1][y + width - 1];
+                    int w2D = int_img_mat[x + height - 1][y];
+                    int white2 = w2C + w2A - w2B - w2D;
 
-                return (black2-white2)/(width*height);
-            case 3:
-                // 3 = [1,0,1]
-                int b3Al = int_img_mat[x][y];
-                int b3Bl = int_img_mat[x][y+(width/3)-1];
-                int b3Cl = int_img_mat[x+height-1][y+(width/3)-1];
-                int b3Dl = int_img_mat[x+height-1][y];
-                int black3l = b3Cl+b3Al-b3Bl-b3Dl;
+                    return (black2 - white2) / (width * height);
+                case 3:
+                    // 3 = [1,0,1]
+                    // FOR SCALE 1 CASE
+                    if(width == 3 && height == 1){
+                        return (int_img_mat[x][y]+int_img_mat[x][y+2])-int_img_mat[x][y+1];
+                    }
+                    int b3Al = int_img_mat[x][y];
+                    int b3Bl = int_img_mat[x][y + (width / 3) - 1];
+                    int b3Cl = int_img_mat[x + height - 1][y + (width / 3) - 1];
+                    int b3Dl = int_img_mat[x + height - 1][y];
+                    int black3l = b3Cl + b3Al - b3Bl - b3Dl;
 
-                int w3A = int_img_mat[x][y+(width/3)];
-                int w3B = int_img_mat[x][y+2*(width/3)-1];
-                int w3C = int_img_mat[x-height-1][y+2*(width/3)-1];
-                int w3D = int_img_mat[x-height-1][y+(width/3)];
-                int white3 = w3C+w3A-w3B-w3D;
+                    int w3A = int_img_mat[x][y + (width / 3)];
+                    int w3B = int_img_mat[x][y + 2 * (width / 3) - 1];
+                    int w3C = int_img_mat[x - height - 1][y + 2 * (width / 3) - 1];
+                    int w3D = int_img_mat[x - height - 1][y + (width / 3)];
+                    int white3 = w3C + w3A - w3B - w3D;
 
-                int b3Ar = int_img_mat[x][y+2*(width/3)];
-                int b3Br = int_img_mat[x][y+width-1];
-                int b3Cr = int_img_mat[x-height-1][y+width-1];
-                int b3Dr = int_img_mat[x-height-1][y+2*(width/3)];
-                int black3r = b3Cr+b3Ar-b3Br-b3Dr;
+                    int b3Ar = int_img_mat[x][y + 2 * (width / 3)];
+                    int b3Br = int_img_mat[x][y + width - 1];
+                    int b3Cr = int_img_mat[x - height - 1][y + width - 1];
+                    int b3Dr = int_img_mat[x - height - 1][y + 2 * (width / 3)];
+                    int black3r = b3Cr + b3Ar - b3Br - b3Dr;
 
-                return ((black3l+black3r)-white3)/(width*height);
-            case 4:
-                // 4 = [1]
-                //     [0]
-                //     [1]
-                int b4At = int_img_mat[x][y];
-                int b4Bt = int_img_mat[x][y+width-1];
-                int b4Ct = int_img_mat[x+(height/3)-1][y+width-1];
-                int b4Dt = int_img_mat[x+(height/3)-1][y];
-                int black4t = b4Ct+b4At-b4Bt-b4Dt;
+                    return ((black3l + black3r) - white3) / (width * height);
+                case 4:
+                    // 4 = [1]
+                    //     [0]
+                    //     [1]
+                    // FOR SCALE 1 CASE
+                    if(width == 1 && height == 3){
+                        return (int_img_mat[x][y]+int_img_mat[x+2][y])-int_img_mat[x+1][y];
+                    }
+                    int b4At = int_img_mat[x][y];
+                    int b4Bt = int_img_mat[x][y + width - 1];
+                    int b4Ct = int_img_mat[x + (height / 3) - 1][y + width - 1];
+                    int b4Dt = int_img_mat[x + (height / 3) - 1][y];
+                    int black4t = b4Ct + b4At - b4Bt - b4Dt;
 
-                int w4A = int_img_mat[x+(height/3)][y];
-                int w4B = int_img_mat[x+(height/3)][y+width-1];
-                int w4C = int_img_mat[x+2*(height/3)-1][y+width-1];
-                int w4D = int_img_mat[x+2*(height/3)-1][y];
-                int white4 = w4C+w4A-w4B-w4D;
+                    int w4A = int_img_mat[x + (height / 3)][y];
+                    int w4B = int_img_mat[x + (height / 3)][y + width - 1];
+                    int w4C = int_img_mat[x + 2 * (height / 3) - 1][y + width - 1];
+                    int w4D = int_img_mat[x + 2 * (height / 3) - 1][y];
+                    int white4 = w4C + w4A - w4B - w4D;
 
-                int b4Ab = int_img_mat[x+2*(height/3)][y];
-                int b4Bb = int_img_mat[x+2*(height/3)][y+width-1];
-                int b4Cb = int_img_mat[x+height-1][y+width-1];
-                int b4Db = int_img_mat[x+height-1][y];
-                int black4b = b4Cb+b4Ab-b4Bb-b4Db;
+                    int b4Ab = int_img_mat[x + 2 * (height / 3)][y];
+                    int b4Bb = int_img_mat[x + 2 * (height / 3)][y + width - 1];
+                    int b4Cb = int_img_mat[x + height - 1][y + width - 1];
+                    int b4Db = int_img_mat[x + height - 1][y];
+                    int black4b = b4Cb + b4Ab - b4Bb - b4Db;
 
-                return ((black4t+black4b)-white4)/(width*height);
-            case 5:
-                // 5 = [1,0]
-                //     [0,1]
-                int b5At = int_img_mat[x][y];
-                int b5Bt = int_img_mat[x][y+(width/2)-1];
-                int b5Ct = int_img_mat[x+(height/2)-1][y+(width/2)-1];
-                int b5Dt = int_img_mat[x+(height/2)-1][y];
-                int black5t = b5Ct+b5At-b5Bt-b5Dt;
+                    return ((black4t + black4b) - white4) / (width * height);
+                case 5:
+                    // 5 = [1,0]
+                    //     [0,1]
+                    // FOR SCALE 1 CASE
+                    if(width == 2 && height == 2){
+                        return (int_img_mat[x][y]+int_img_mat[x+1][y+1])-(int_img_mat[x][y+1]+int_img_mat[x+1][y]);
+                    }
+                    int b5At = int_img_mat[x][y];
+                    int b5Bt = int_img_mat[x][y + (width / 2) - 1];
+                    int b5Ct = int_img_mat[x + (height / 2) - 1][y + (width / 2) - 1];
+                    int b5Dt = int_img_mat[x + (height / 2) - 1][y];
+                    int black5t = b5Ct + b5At - b5Bt - b5Dt;
 
-                int w5At = int_img_mat[x][y+(width/2)];
-                int w5Bt = int_img_mat[x][y+width-1];
-                int w5Ct = int_img_mat[x+(height/2)-1][y+width-1];
-                int w5Dt = int_img_mat[x+(height/2)-1][y+(width/2)];
-                int white5t = w5Ct+w5At-w5Bt-w5Dt;
+                    int w5At = int_img_mat[x][y + (width / 2)];
+                    int w5Bt = int_img_mat[x][y + width - 1];
+                    int w5Ct = int_img_mat[x + (height / 2) - 1][y + width - 1];
+                    int w5Dt = int_img_mat[x + (height / 2) - 1][y + (width / 2)];
+                    int white5t = w5Ct + w5At - w5Bt - w5Dt;
 
-                int w5Ab = int_img_mat[x+(height/2)][y];
-                int w5Bb = int_img_mat[x+(height/2)][y+(width/2)-1];
-                int w5Cb = int_img_mat[x+height-1][y+(width/2)-1];
-                int w5Db = int_img_mat[x+height-1][y];
-                int white5b = w5Cb+w5Ab-w5Bb-w5Db;
+                    int w5Ab = int_img_mat[x + (height / 2)][y];
+                    int w5Bb = int_img_mat[x + (height / 2)][y + (width / 2) - 1];
+                    int w5Cb = int_img_mat[x + height - 1][y + (width / 2) - 1];
+                    int w5Db = int_img_mat[x + height - 1][y];
+                    int white5b = w5Cb + w5Ab - w5Bb - w5Db;
 
-                int b5Ab = int_img_mat[x+(height/2)][y+(width/2)];
-                int b5Bb = int_img_mat[x+(height/2)][y+width-1];
-                int b5Cb = int_img_mat[x+height-1][y+width-1];
-                int b5Db = int_img_mat[x+height-1][y+(width/2)];
-                int black5b = b5Cb+b5Ab-b5Bb-b5Db;
+                    int b5Ab = int_img_mat[x + (height / 2)][y + (width / 2)];
+                    int b5Bb = int_img_mat[x + (height / 2)][y + width - 1];
+                    int b5Cb = int_img_mat[x + height - 1][y + width - 1];
+                    int b5Db = int_img_mat[x + height - 1][y + (width / 2)];
+                    int black5b = b5Cb + b5Ab - b5Bb - b5Db;
 
-                return ((black5t+black5b)-(white5t+white5b))/(width*height);
+                    return ((black5t + black5b) - (white5t + white5b)) / (width * height);
+            }
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Unable to compute: " + "width:"+width+" height:"+height );
         }
         return 0;
     }
